eebedb451e15bbd1852a05d136f84d56db443aa7
Дарья <dasha.seredenko.1997@gmail.com>
2021-10-06 23:38:14 +0400

    Добавлена Work Details

--- src/components/works/worksDetails/WorkDetails.tsx
+++ src/components/works/worksDetails/WorkDetails.tsx
@@ -3,21 +3,51 @@ import {useParams} from "react-router-dom";
 import styles from "./work_details.scss"
 import Panel from "../../shared/headerPanel/Panel";
 import DateInfo from "../../shared/date_info/DateInfo";
+import Footer from "../../shared/footer/Footer";
+
+import team from "../../../img/team_diversity.png";
+import car from "../../../img/car.png";
+import timetable from "../../../img/timetable.png";
 
 const WorkDetails = () => {
     let {id} = useParams<{ readonly id: string }>();
     return (
         <div className={styles.root}>
             <Panel/>
-            <div className={styles.header}>
-                Designing Dashboards with <br/> usability in mind
+            <div className={styles.container}>
+                <h1 className={styles.header}>
+                    Designing Dashboards with usability in mind
+                </h1>
+                <div>
+                    <DateInfo
+                        year={"2020"}
+                        type={"Dashboard, User Experience Design"}
+                    />
+                </div>
+                <div className={styles.desc}>
+                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                </div>
+                <div className={styles.image}>
+                    <img src={team}/>
+                </div>
+                <h2 className={styles.heading1}>
+                    Heading 1
+                </h2>
+                <h3 className={styles.heading2}>
+                    Heading 2
+                </h3>
+                <div className={styles.desc}>
+                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                </div>
+                <div className={styles.image}>
+                    <img src={car}/>
+                </div>
+                <div className={styles.image}>
+                    <img src={timetable}/>
+                </div>
             </div>
-            <div>
-                <DateInfo
-                    year={"2020"}
-                    type={"Dashboard, User Experience Design"}
-                />
-            </div>
+
+            <Footer/>
         </div>);
 };
 
